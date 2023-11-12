20a96145ec0dde8964b24990b1caec9b6cc695c8
nutsa.matchavariani <nutsa.matchavariani.1@gmail.com>
2023-11-12 20:47:32 +0400

    module 3 task 3

--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        var first=1;
+        var second = 10;
+        var third = 100;
+
+        var linkToFirst = first;
+        var linkToSecond = second;
+        var linkToThird = third;
+        first = 15;
+        second = 6;
+        third=4;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
     }
 }
