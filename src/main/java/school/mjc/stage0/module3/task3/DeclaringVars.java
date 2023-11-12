773623031be9b09695504ed3ff6767457111e553
nutsa.matchavariani <nutsa.matchavariani.1@gmail.com>
2023-11-12 21:53:43 +0400

    module 3 task 3''

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -2,7 +2,9 @@ package school.mjc.stage0.module3.task3;
 
 public class DeclaringVars {
     public static void main(String[] args) {
-        int a = 10; int b = a; int c=b;
+        int a, b, c;
+    a = b = c=10;
+
         System.out.println(c);
     }
 }
